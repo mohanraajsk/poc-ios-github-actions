e45fb4b39ecf15f0a6027faebee575f5fe0343c5
Mohanraj S K <sk_mohanraj@lilly.com>
2022-05-24 12:49:42 +0530

    Initial commit

---
+++ POC_GithuAction/POC_GithuActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_GithuActionApp.swift
+//  POC_GithuAction
+//
+//  Created by Mohanraj SK on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct POC_GithuActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
